cbd52a08445662d56e86b219d4ee2451acfbfc22
Matthes <Matthes@AWI-NB032.ad.uni-heidelberg.de>
2024-05-15 09:52:47 +0200

    treatments

--- main_survey/__init__.py
+++ main_survey/__init__.py
@@ -108,7 +108,7 @@ class Player(BasePlayer):
 def creating_session(subsession: Subsession):
     import itertools
     import random
-    treatments = itertools.cycle([6,7]) #NNH, NHH, NH, BNH, BHH, BH, SenNB, SenB
+    treatments = itertools.cycle([7]) #itertools.cycle([0, 0, 0,  1, 1, 2, 3, 3, 3, 4, 5, 5])
     for player in subsession.get_players():
         player.participant.treatment = next(treatments)
         player.treatment = player.participant.treatment
